5f2a7d77ebed20b49b9b0cf1ff8c2b71bedadaaa
Letícia Farias <lf182000@gmail.com>
2025-05-23 14:03:50 -0300

    finish css icon (?)

--- src/components/CollectionIcons/CollectionIcons.jsx
+++ src/components/CollectionIcons/CollectionIcons.jsx
@@ -9,24 +9,28 @@ export default function CollectionIcons() {
         <h2 className={styles.sectionTitle}>Coleções em destaque</h2>
 
         <div className={styles.iconGrid}>
-          <Link to="/categoria/camisetas" className={styles.iconItem}>
-          <img src="./src/assets/tshirt.svg" alt="t-shirt" />
+          <Link to="/categoria/camisetas" className={`${styles.iconItem} ${styles.active}`}>
+            <img src="/src/assets/tshirt.svg" alt="Camisetas" className={styles.iconImage} />
             <span className={styles.categoryName}>Camisetas</span>
           </Link>
+
           <Link to="/categoria/calcas" className={styles.iconItem}>
-            <img src="./src/assets/pants.svg" alt="pants" />
+            <img src="/src/assets/pants.svg" alt="Calças" className={styles.iconImage} />
             <span className={styles.categoryName}>Calças</span>
           </Link>
+
           <Link to="/categoria/bones" className={styles.iconItem}>
-            <img src="./src/assets/cap.svg" alt="cap" />
+            <img src="/src/assets/baseball-cap.svg" alt="Bonés" className={styles.iconImage} />
             <span className={styles.categoryName}>Bonés</span>
           </Link>
+
           <Link to="/categoria/headphones" className={styles.iconItem}>
-          <img src="./src/assets/headphone.svg" alt="headphone" />
+            <img src="/src/assets/headphone.svg" alt="Headphones" className={styles.iconImage} />
             <span className={styles.categoryName}>Headphones</span>
           </Link>
+
           <Link to="/categoria/tenis" className={styles.iconItem}>
-          <img src="./src/assets/sneaker.svg" alt="sneaker" />
+            <img src="/src/assets/sneaker.svg" alt="Tênis" className={styles.iconImage} />
             <span className={styles.categoryName}>Tênis</span>
           </Link>
         </div>
