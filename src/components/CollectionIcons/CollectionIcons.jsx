3617d6d84ff70de9979fb8ab83ce4ae48d242f5d
Letícia Farias <lf182000@gmail.com>
2025-05-23 13:31:56 -0300

    edit css icon

--- src/components/CollectionIcons/CollectionIcons.jsx
+++ src/components/CollectionIcons/CollectionIcons.jsx
@@ -4,34 +4,30 @@ import styles from './CollectionIcons.module.css'
 
 export default function CollectionIcons() {
   return (
-    <section className="collectionIcons">
+    <section className={styles.collectionIcons}>
       <div className="container">
-        <h2 className="section-title">Coleções em destaque</h2>
+        <h2 className={styles.sectionTitle}>Coleções em destaque</h2>
 
-        <div className="iconGrid">
-          <Link to="/categoria/camisetas" className="iconItem">
+        <div className={styles.iconGrid}>
+          <Link to="/categoria/camisetas" className={styles.iconItem}>
             <span className="material-symbols-outlined">tshirt</span>
-            <span className="category-name">Camisetas</span>
+            <span className={styles.categoryName}>Camisetas</span>
           </Link>
-
-          <Link to="/categoria/calcas" className="iconItem">
+          <Link to="/categoria/calcas" className={styles.iconItem}>
             <span className="material-symbols-outlined">checkroom</span>
-            <span className="category-name">Calças</span>
+            <span className={styles.categoryName}>Calças</span>
           </Link>
-
-          <Link to="/categoria/bones" className="iconItem">
+          <Link to="/categoria/bones" className={styles.iconItem}>
             <span className="material-symbols-outlined">sports_baseball</span>
-            <span className="category-name">Bonés</span>
+            <span className={styles.categoryName}>Bonés</span>
           </Link>
-
-          <Link to="/categoria/headphones" className="iconItem">
+          <Link to="/categoria/headphones" className={styles.iconItem}>
             <span className="material-symbols-outlined">headphones</span>
-            <span className="category-name">Headphones</span>
+            <span className={styles.categoryName}>Headphones</span>
           </Link>
-
-          <Link to="/categoria/tenis" className="iconItem">
+          <Link to="/categoria/tenis" className={styles.iconItem}>
             <span className="material-symbols-outlined">sneaker</span>
-            <span className="category-name">Tênis</span>
+            <span className={styles.categoryName}>Tênis</span>
           </Link>
         </div>
       </div>
