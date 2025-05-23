4e7e255e94b3a308d638eaf3ae9e42bbf8f80d70
Letícia Farias <lf182000@gmail.com>
2025-05-23 14:08:02 -0300

    jsx icons finish

--- src/components/CollectionIcons/CollectionIcons.jsx
+++ src/components/CollectionIcons/CollectionIcons.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './CollectionIcons.module.css'
 
+import tshirt from '../../assets/tshirt.svg'
+import pants from '../../assets/pants.svg'
+import cap from '../../assets/cap.svg'
+import headphone from '../../assets/headphone.svg'
+import sneaker from '../../assets/sneaker.svg'
+
 export default function CollectionIcons() {
   return (
     <section className={styles.collectionIcons}>
@@ -10,27 +16,27 @@ export default function CollectionIcons() {
 
         <div className={styles.iconGrid}>
           <Link to="/categoria/camisetas" className={`${styles.iconItem} ${styles.active}`}>
-            <img src="../src/assets/tshirt.svg" alt="Camisetas" className={styles.iconImage} />
+            <img src={tshirt} alt="Camisetas" className={styles.iconImage} />
             <span className={styles.categoryName}>Camisetas</span>
           </Link>
 
           <Link to="/categoria/calcas" className={styles.iconItem}>
-            <img src="../src/assets/pants.svg" alt="Calças" className={styles.iconImage} />
+            <img src={pants} alt="Calças" className={styles.iconImage} />
             <span className={styles.categoryName}>Calças</span>
           </Link>
 
           <Link to="/categoria/bones" className={styles.iconItem}>
-            <img src="../src/assets/baseball-cap.svg" alt="Bonés" className={styles.iconImage} />
+            <img src={cap} alt="Bonés" className={styles.iconImage} />
             <span className={styles.categoryName}>Bonés</span>
           </Link>
 
           <Link to="/categoria/headphones" className={styles.iconItem}>
-            <img src="../src/assets/headphone.svg" alt="Headphones" className={styles.iconImage} />
+            <img src={headphone} alt="Headphones" className={styles.iconImage} />
             <span className={styles.categoryName}>Headphones</span>
           </Link>
 
           <Link to="/categoria/tenis" className={styles.iconItem}>
-            <img src="../src/assets/sneaker.svg" alt="Tênis" className={styles.iconImage} />
+            <img src={sneaker} alt="Tênis" className={styles.iconImage} />
             <span className={styles.categoryName}>Tênis</span>
           </Link>
         </div>
