359e3776a9fadd67f03ab80ea7f62fc3d7dd0eb3
Letícia Farias <lf182000@gmail.com>
2025-05-23 13:39:42 -0300

    icons css

--- src/components/CollectionIcons/CollectionIcons.jsx
+++ src/components/CollectionIcons/CollectionIcons.jsx
@@ -10,23 +10,23 @@ export default function CollectionIcons() {
 
         <div className={styles.iconGrid}>
           <Link to="/categoria/camisetas" className={styles.iconItem}>
-            <span className="material-symbols-outlined">tshirt</span>
+            <i className="ph ph-t-shirt"></i>
             <span className={styles.categoryName}>Camisetas</span>
           </Link>
           <Link to="/categoria/calcas" className={styles.iconItem}>
-            <span className="material-symbols-outlined">checkroom</span>
+            <i className="ph ph-pants"></i>
             <span className={styles.categoryName}>Calças</span>
           </Link>
           <Link to="/categoria/bones" className={styles.iconItem}>
-            <span className="material-symbols-outlined">sports_baseball</span>
+            <i className="ph ph-baseball-cap"></i>
             <span className={styles.categoryName}>Bonés</span>
           </Link>
           <Link to="/categoria/headphones" className={styles.iconItem}>
-            <span className="material-symbols-outlined">headphones</span>
+            <i className="ph ph-headphones"></i>
             <span className={styles.categoryName}>Headphones</span>
           </Link>
           <Link to="/categoria/tenis" className={styles.iconItem}>
-            <span className="material-symbols-outlined">sneaker</span>
+            <i className="ph ph-sneaker-move"></i>
             <span className={styles.categoryName}>Tênis</span>
           </Link>
         </div>
