acc9dd69390ba959b5eefe7b18f78a9f366dd72a
Letícia Farias <lf182000@gmail.com>
2025-05-24 00:19:10 -0300

    specialOffer edit

--- src/components/SpecialOffer/SpecialOffer.jsx
+++ src/components/SpecialOffer/SpecialOffer.jsx
@@ -2,29 +2,26 @@ import styles from "../SpecialOffer/SpecialOffer.module.css"
 
 export default function SpecialOffer() {
   return (
-    <section className={styles.specialOfferSection}>
-      <div className={styles.specialOfferContainer}>
-        {/* Círculo gradiente decorativo */}
-        <div className={styles.decorativeCircle}>
+    <section className={styles.specialOffer__section}>
+      <div className={styles.specialOffer__container}>
+        <div className={styles.specialOffer__circle}>
           <img src="/Ellipse.png" alt="" />
         </div>
 
-        {/* Imagem do produto */}
-        <div className={styles.productImageSection}>
-          <img src="/Laye.png" alt="Air Jordan edição de colecionador" className={styles.productImg} />
+        <div className={styles.specialOffer__imageWrapper}>
+          <img src="/Laye.png" alt="Air Jordan edição de colecionador" className={styles.specialOffer__image} />
         </div>
 
-        {/* Conteúdo da oferta */}
-        <div className={styles.offerContentSection}>
-          <div className={styles.offerCard}>
-            <span className={styles.specialBadge}>Oferta especial</span>
-            <h2 className={styles.specialTitle}>Air Jordan edição de colecionador</h2>
-            <p className={styles.specialDescription}>
+        <div className={styles.specialOffer__content}>
+          <div className={styles.specialOffer__card}>
+            <span className={styles.specialOffer__badge}>Oferta especial</span>
+            <h2 className={styles.specialOffer__title}>Air Jordan edição de colecionador</h2>
+            <p className={styles.specialOffer__description}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
               ea.
             </p>
-            <a className={styles.specialButton} href="/oferta">
+            <a className={styles.specialOffer__button} href="/oferta">
               Ver Oferta
             </a>
           </div>
