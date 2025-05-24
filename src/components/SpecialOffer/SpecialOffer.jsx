18f466de5122c044bf4cb020fcdff146c25ab817
Letícia Farias <lf182000@gmail.com>
2025-05-24 00:10:08 -0300

    specialOffer edit

--- src/components/SpecialOffer/SpecialOffer.jsx
+++ src/components/SpecialOffer/SpecialOffer.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import styles from '../SpecialOffer/SpecialOffer.module.css'
+import styles from "../SpecialOffer/SpecialOffer.module.css"
 
 export default function SpecialOffer() {
   return (
-    <section className={styles.SpecialOffer}>
-      <div className={styles['offer-container']}>
-
-        <div className={styles['offer-image']}>
-          <img
-            alt="Air Jordan edição de colecionador"
-            loading="lazy"
-            decoding="async"
-            src="/Laye.png"
-          />
+    <section className={styles.specialOffer}>
+      <div className={styles.container}>
+        {/* Elemento decorativo - círculo gradiente */}
+        <div className={styles.gradientCircle}>
+          <img src="/ellipse-gradient.png" alt="" />
         </div>
 
-        <div className={styles['offer-content']}>
-          <span className={styles['offer-badge']}>Oferta especial</span>
-          <h2 className={styles['offer-title']}>Air Jordan edição de colecionador</h2>
-          <p className={styles['offer-description']}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.
-          </p>
-          <a className={styles.button} href="/oferta">Ver oferta</a>
+        {/* Imagem do produto */}
+        <div className={styles.productImage}>
+          <img src="/Laye.png" alt="Air Jordan edição de colecionador" className={styles.sneakerImage} />
         </div>
 
+        {/* Conteúdo da oferta */}
+        <div className={styles.offerContent}>
+          <div className={styles.contentWrapper}>
+            <span className={styles.offerBadge}>Oferta especial</span>
+            <h2 className={styles.offerTitle}>Air Jordan edição de colecionador</h2>
+            <p className={styles.offerDescription}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea.
+            </p>
+            <a className={styles.offerButton} href="/oferta">
+              Ver Oferta
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   )
