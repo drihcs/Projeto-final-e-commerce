5db95545fdec853afd552a3fdb73b1620f6eb3e2
Adriana Silva <drih1502@gmail.com>
2025-05-31 18:04:30 -0300

    unificando supabase em apenas um projeto

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,20 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrlProdutos = import.meta.env.VITE_SUPABASE_URL_PRODUTOS;
-const supabaseAnonKeyProdutos = import.meta.env.VITE_SUPABASE_ANON_KEY_PRODUTOS;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabaseProdutos = createClient(supabaseUrlProdutos, supabaseAnonKeyProdutos, {
-  auth: {
-    storageKey: 'supabase.auth.produtos' // chave exclusiva para armazenamento local
-  }
-});
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-
-const supabaseUrlUsuarios = import.meta.env.VITE_SUPABASE_URL_USUARIOS;
-const supabaseAnonKeyUsuarios = import.meta.env.VITE_SUPABASE_ANON_KEY_USUARIOS;
-
-export const supabaseUsuarios = createClient(supabaseUrlUsuarios, supabaseAnonKeyUsuarios, {
-  auth: {
-    storageKey: 'supabase.auth.usuarios' // chave exclusiva para armazenamento local
-  }
-});
+export default supabase;
