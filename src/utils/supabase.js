b2d84266006053c2352d54c67b801d0c1c5aff8c
Adriana Silva <drih1502@gmail.com>
2025-05-31 18:21:44 -0300

    atualizando supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -3,10 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-<<<<<<< HEAD
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
-=======
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
->>>>>>> 325bc26fff4ffa20cedfded5dff2fdc98ef8c555
