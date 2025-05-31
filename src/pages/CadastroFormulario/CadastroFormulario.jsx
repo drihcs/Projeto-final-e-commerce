6f375311b935aac3565d38d6465531a4740dcba9
Adriana Silva <drih1502@gmail.com>
2025-05-31 14:10:59 -0300

    correção linkagem header

--- src/pages/CadastroFormulario/CadastroFormulario.jsx
+++ src/pages/CadastroFormulario/CadastroFormulario.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 import styles from './CadastroFormulario.module.css';
 
 function CadastroFormulario() {
