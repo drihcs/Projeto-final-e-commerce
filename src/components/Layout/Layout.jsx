ecca52bf26abeee3da5c263185c0213094fe21b0
S A R A <123466976+sarasmorais@users.noreply.github.com>
2025-06-09 19:55:40 -0300

    final

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
-import Breadcrumb from '../Breadcrumbs/Breadcrumbs'i
+import Breadcrumb from '../Breadcrumbs/Breadcrumbs'
 import styles from '../Layout/Layout.module.css'
 
 export default function Layout() {
